317715420df94d5a7a7ef0823d659d2065e59ac6
Melike Vurucu <melikechan@tuta.io>
2023-12-05 22:16:30 +0300

    add: problem template files

---
+++ single_test_case_template.cpp
@@ -0,0 +1,89 @@
+/*
+    Author: Melike Vurucu (melikechan)
+    I hope I won't get confused.
+*/
+
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("sse4,avx,avx2,bmi,bmi2,popcnt,lzcnt") // default architechture flags, you can use below as fallback
+// #pragma GCC target("sse4,bmi,bmi2,popcnt,lzcnt")
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+// Loops
+#define loopi($i, s, es, n, inc) for (ll $i = s; es < n; $i += inc)
+#define loopieq($i, s, es, n, inc) for (ll $i = s; es <= n; $i += inc)
+#define loopd($i, s, es, n, dec) for (ll $i = s; es > n; $i -= dec)
+#define loopdeq($i, s, es, n, dec) for (ll $i = s; es >= n; $i -= dec)
+
+/*
+    Pairs and tuples
+    i -> int (for every int, there is a corresponding i)
+    l -> long long
+*/
+typedef pair<int, int> pii;
+typedef tuple<int, int, int> piii;
+typedef tuple<int, int, int, int> piiii;
+typedef tuple<int, int, int, int, int> piiiii;
+typedef pair<ll, ll> pll;
+typedef tuple<ll, ll, ll> plll;
+typedef tuple<ll, ll, ll, ll> pllll;
+typedef tuple<ll, ll, ll, ll, ll> plllll;
+
+/*
+    Nested vectors
+    v -> vector (for every level of nesting, there is a corresponding v)
+    vi -> vector<int>
+    vl -> vector<long long>
+*/
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<vvvi> vvvvi;
+typedef vector<vvvvi> vvvvvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<vvl> vvvl;
+typedef vector<vvvl> vvvvl;
+typedef vector<vvvvl> vvvvvl;
+
+// Constants
+constexpr ll mod = ll(1e9) + 7;
+constexpr ll inf = 2e18;
+constexpr int intinf = INT_MAX - 1e3;
+constexpr ll mod = ll(1e9) + 7;
+constexpr ll inf = 2e18;
+constexpr int intinf = INT_MAX - 1e3;
+
+inline ll lsb(ll x)
+{
+    return (x) & (-x);
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    for (auto &i : v)
+    {
+        os << i << " ";
+    }
+    return os;
+}
+
+void solve()
+{
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    solve();
+    return 0;
+}
